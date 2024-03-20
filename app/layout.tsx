8d9288416fb1f0a1bead1b30af6d2f7d2f396f78
beerose <alexsandra.sikora@gmail.com>
2024-03-20 16:35:15 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
       <body className={inter.className}>
         <AI>{children}</AI>
       </body>
